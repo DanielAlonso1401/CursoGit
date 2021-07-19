55c728fda0e16748a84986c6c290d4342851adbf
Daniel A <danielbvothsolutions.com>
2021-07-19 11:57:55 -0500

    Cmabios 1

---
+++ prubagit.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class pruebagit
+{
+  public static void main(String [] args)
+  {
+    JOptionPane.showMessageDialog(null, "Hola we");
+  }
+}
